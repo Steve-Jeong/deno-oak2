df325a1afafb2e35c0e96d4ef8edd70bf3418f01
Steve Jeong <jst0930@gmail.com>
2022-12-08 08:53:38 +0300

    bootstrap, ejs, static files included

--- main.ts
+++ main.ts
@@ -1,6 +1,24 @@
-import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+import { Application, Router } from "https://deno.land/x/oak@v11.1.0/mod.ts";
+// import { staticFileMiddleware } from "./components/staticFileMiddleware.ts";
+import {
+  ejsEngine,
+  oakAdapter,
+  viewEngine,
+} from "https://deno.land/x/view_engine@v10.5.1/mod.ts";
 
 const app = new Application();
+
+// app.use(staticFileMiddleware)
+
+app.use(
+  viewEngine(
+    oakAdapter,
+    ejsEngine,
+    {
+      viewRoot: "views",
+    },
+  ),
+);
 
 // Logger
 app.use(async (ctx, next) => {
@@ -17,36 +35,21 @@ app.use(async (ctx, next) => {
   ctx.response.headers.set("X-Response-Time", `${ms}ms`);
 });
 
-// Hello World!
+// Hello World Middleware
 app.use((ctx, next) => {
-  ctx.response.body = "Hello World!";
+  ctx.response.body = "Hello World Middleware!";
   next()
 });
 
 const router = new Router();
 router
-  .get("/", (ctx) => {
-    ctx.response.body = `<!DOCTYPE html>
-    <html>
-      <head><title>Hello oak!</title><head>
-      <body>
-        <a href='/'>Home</a><a href='/about'>About</a>
-        <h1>Home</h1>
-      </body>
-    </html>
-  `;
-  })
-  .get("/about", (ctx) => {
-    ctx.response.body = `<!DOCTYPE html>
-    <html>
-      <head><title>Hello oak!</title><head>
-      <body>
-        <a href='/'>Home</a><a href='/about'>About</a>
-        <h1>About</h1>
-      </body>
-    </html>`;
-  })
-  
+.get("/", async (ctx, next) => {
+  ctx.render('index.ejs', {name: '정상태'})
+})
+.get("/about", async (ctx, next) => {
+  ctx.render('about.ejs', {name: '관하여'})
+})
+
 
 app.use(router.routes());
 app.use(router.allowedMethods());
@@ -54,3 +57,22 @@ app.use(router.allowedMethods());
 const PORT = 8000
 console.log(`Server listening on http://localhost:${PORT}`)
 await app.listen({ port: PORT });
+
+
+// // app.ts
+// import { Application } from "https://deno.land/x/oak@v10.5.1/mod.ts";
+// import { viewEngine, etaEngine, oakAdapter } from "https://deno.land/x/view_engine@v10.5.1c/mod.ts"
+
+// const app = new Application();
+
+// app.use(
+//   viewEngine(oakAdapter, etaEngine, {
+//     viewRoot: "./views",
+//   })
+// );
+
+// app.use(async (ctx, next) => {
+//   ctx.render("index.eta", { name: "John" } );
+// });
+
+// await app.listen({ port: 8000 });
